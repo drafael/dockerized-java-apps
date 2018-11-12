17d8d4ea66a5e34af6ee1dcb6decbd52b838f971
D.R4F4EL <d.r4f4el@gmail.com>
2018-11-12 16:34:04 +0200

    Using Java 8 flags for Docker

--- legacy-java-webapp/src/main/docker/docker-entrypoint.sh
+++ legacy-java-webapp/src/main/docker/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [ "$(id -u)" = '0' ]; then       # current user is root
-    chown -R tomcat:tomcat .        # fix permissions
-	exec su-exec tomcat "$0" "$@"   # step-down from root
+if [ "$(id -u)" = '0' ]; then                # current user is root
+  chown -R tomcat:tomcat $CATALINA_HOME      # fix permissions
+  exec su-exec tomcat "$0" "$@"              # step-down from root
 fi
 
 exec "$@"
--- modern-java-webapp/src/main/docker/docker-entrypoint.sh
+++ modern-java-webapp/src/main/docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$(id -u)" = '0' ]; then       # current user is root
-	exec su-exec nobody "$0" "$@"   # step-down from root
+    exec su-exec nobody "$0" "$@"   # step-down from root
 fi
 
 exec "$@"
