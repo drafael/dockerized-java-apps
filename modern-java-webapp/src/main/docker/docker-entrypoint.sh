073f227c22f0497bb9c12b4c908886f93c2c0d02
D.R4F4EL <d.r4f4el@gmail.com>
2019-05-23 20:14:51 +0300

    updated

--- modern-java-webapp/src/main/docker/docker-entrypoint.sh
+++ modern-java-webapp/src/main/docker/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ "$(id -u)" = '0' ]; then                # current user is root
-    exec su-exec "$USER_NAME" "$0" "$@"      # step-down from root
+if [ "$(id -u)" = '0' ]; then                  # current user is root
+    exec su-exec "${USER_NAME}" "$0" "$@"      # step-down from root
 fi
 
 exec "$@"
